67a939ecdd610788d44d7157fcd2338baabe9da1
jaxx-era <koenn.jhon@gmail.com>
2026-03-02 16:00:38 +0530

    Create verifyPayment.ts

---
+++ lib/verifyPayment.ts
@@ -0,0 +1,16 @@
+import crypto from "crypto"
+
+export function verifySignature(
+  orderId: string,
+  paymentId: string,
+  signature: string
+) {
+  const body = orderId + "|" + paymentId
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET!)
+    .update(body.toString())
+    .digest("hex")
+
+  return expectedSignature === signature
+}
